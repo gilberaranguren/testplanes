84ddebabaeed0a86737da53cf24583c6c6c1b49c
gilberaranguren <gilberaranguren@gmail.com>
2021-04-18 16:02:10 -0400

    backend parte 2

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,23 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-
+const cors = require('cors');
+app.use(cors());
 // respond with "hello world" when a GET request is made to the homepage
 
-app.get('/planes', async (req, res) => {
+app.get('/planes/:renta', async (req, res) => {
   try {
+
+     const renta = req.params.renta;
+     const rentaEnNumero = parseInt(renta,10);
+     const porcentaje = rentaEnNumero*0.15;
+     const rentaSuperior = rentaEnNumero+porcentaje;
+     const rentaInferior = rentaEnNumero-porcentaje;
+
+     console.log("Consultando planes");
      const {data} = await axios.get('https://f3ec8e44-0529-430b-8433-d492dba0797c.mock.pstmn.io/planes');
-     res.send(data);
+     const planes = data.filter((plan)=> plan.precio >= rentaInferior && plan.precio <= rentaSuperior )
+     res.send(planes);
   } catch (error){
      res.json(error);
   } 
@@ -16,7 +26,7 @@ app.get('/planes', async (req, res) => {
 });
 
 app.post('/', function(req, res) {
-  res.send('hello world Gilber');
+  res.send('');
 });
 
-app.listen(3000);
+app.listen(3001);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { Row, Col, Form, Input, Label, FormGroup, Button, FormFeedback } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rut: '',
+      renta: '',
+      mensajeRut: '',
+      mensajeRenta: '',
+      invalidRut: false,
+      invalidRenta: false,
+      planes: [],
+      selectedPlan: '',
+
+
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onClick = this.onClick.bind(this);
+
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    const request = {
+      selectedPlan: this.state.selectedPlan,
+      rut: this.state.rut,
+    }
+    //Enviar formulario 
+    console.log("Se envian los datos " + JSON.stringify(request));
+
+  }
+
+  async onClick() {
+    const myHeaders = new Headers();
+
+    const myInit = {
+      method: 'GET',
+      headers: myHeaders,
+      //mode: 'cors',
+      cache: 'default'
+    };
+
+    const myRequest = new Request(`http://localhost:3001/planes/${this.state.renta}`, myInit);
+
+    const response = await fetch(myRequest);
+    const planes = await response.json();
+    this.setState({
+      planes
+    })
+    //console.log(response.json());
+  }
+
+  onChange = async e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+    console.log(name, value);
+    //await this.getPlanes();
+  }
+
+  
+  
+
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col xs="3"></Col>
+          <Col xs="6">
+            <h2>Busqueda de Planes</h2>
+            <Form onSubmit={this.onSubmit}>
+              <FormGroup>
+                <Label>Rut</Label>
+                <Input type="text" name="rut" value={this.state.rut} onChange={this.onChange} />
+
+              </FormGroup>
+              <FormGroup>
+                <Label>Renta</Label>
+                <Input type="text" name="renta" value={this.state.renta} onChange={this.onChange}  />
+                <Button color="link" onClick={this.onClick}>Planes</Button>
+              </FormGroup>
+              {
+                this.state.planes.map(key => (
+                  <FormGroup check>
+                    <Label check>
+                      <Input type="radio" name="selectedPlan" onChange={this.onChange} value={key.CodigoPlan} />{' '}
+                      Nombre: {key.Nombre} - Precio: {key.precio}
+                    </Label>
+                  </FormGroup>
+                ))
+              }
+
+              <FormGroup>
+                <Button color="success">Enviar</Button>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+
+      </div>
+
+    )
+  }
+
+
 }
 
 export default App;
